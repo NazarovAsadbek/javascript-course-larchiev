995c771dc41cf1fde3e404220b83597ba7a5c5e9
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2022-11-17 19:39:47 +0500

    Chapter 1

--- app.js
+++ app.js
@@ -1,1 +1,73 @@
-console.log('Hello world!')
+/*  
+	let name1 = 'Asadbek';
+	let lastName1 = 'Nazarov';
+	const name2 = 'Artur';
+	const lastname2 = 'Pirojkov';
+
+console.log('Hello world!', lastName1, name1, name2, lastname2);
+*/
+
+/*
+Операторы
+  
+Оператор - это внутренняя функция в JavaScript
+ 
+Операторы бывают:
+   1. Операторы присваивания
+   2. Операторы сравнения
+   3. Арифметические Операторы
+   4. Битовые Операторы
+   5. Логические Операторы
+   6. Строковые Операторы
+   7. Тернарный Оператор
+   8. Оператор запятая
+   9. Унарные Операторы
+   10. Операторы отношения
+
+*/
+
+/*3.Арифметические операторы
+	const [width, height] = [10, 5];
+	const space = width * height;
+	const volume = 2 ** 3; // Степень
+*/
+
+/*6.Арифметические операторы
+	const city = 'Tashkent';
+	const street = 'Chilanzar';
+	const home = 5;
+*/
+
+/*6. Оператор присваивания
+	let age = 22;
+	age += 1; // Это короткая agge = age + 1;
+	age *= 2; // Это короткая agge = age * 2;
+	age /= 2; // Это короткая agge = age / 2;
+
+	age++ // age = age + 1
+	age-- // age = age - 1
+*/
+
+/*6. Операторы сравнения
+	const vasya = 20;
+
+	console.log(23 > vasya);
+	console.log(23 >= vasya);
+	console.log(23 < vasya);
+	console.log(23 <= vasya);
+	console.log(23 == vasya);
+	console.log(23 === vasya);
+*/
+
+
+/*10. Операторы отношения
+	let a;
+	let b;
+	c = b = 41 + 1;
+	console.log(c);
+	console.log(b);
+*/
+
+
+const age = 22; // Примитив === Не мутабелен. При изменение, предыдущее значение очищатся и записывается новое
+const user = {name: 'Asadbek', age: '22'} // Объекты == Мутабельны 
